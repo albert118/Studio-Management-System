edec5c114cb44483b70a77db826bb6b7db5ab665
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-02 19:36:47 +1100

    added core layer

---
+++ src/server/StudioManagementSystem.Core/Entities/MySpecialObject.cs
@@ -0,0 +1,11 @@
+﻿namespace StudioManagementSystem.Core.Entities;
+
+/// <summary>
+/// An example entity for this project
+/// </summary>
+public class MySpecialObject
+{
+    public Guid Id { get; set; }
+
+    public string SpecialField { get; set; }
+}
