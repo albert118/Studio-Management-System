9032df96ee245802d14332be98a85507ac8036e4
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-02 19:43:39 +1100

    AsyncDbContext and folder placeholders

---
+++ src/server/StudioManagementSystem.Infrastructure/AsyncDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudioManagementSystem.Infrastructure;
+
+public abstract class AsyncDbContext : DbContext
+{
+    protected AsyncDbContext(DbContextOptions options) : base(options) { }
+
+    public override int SaveChanges(bool _) => SaveChanges();
+
+    public override int SaveChanges() => throw new NotSupportedException("Async is preferred over sync calls");
+
+    public override Task<int> SaveChangesAsync(CancellationToken ct = default) => SaveChangesAsync(true, ct);
+
+    public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken ct = default)
+    {
+        if (ct == default)
+        {
+            throw new InvalidOperationException(
+                $"A cancellation token is required when using {nameof(AsyncDbContext)} async methods to allow operation termination mid-process");
+        }
+
+        return await base.SaveChangesAsync(acceptAllChangesOnSuccess, ct);
+    }
+}
