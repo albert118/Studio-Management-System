b1907936095aeb10f02ab08ff7c330ff18118b83
William To <williamto222@gmail.com>
2023-03-18 16:20:35 +1100

    Added react routing and carbon design system

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Project from "./pages/Project/Project";
+import Group from "./pages/Group/Group";
+import Layout from "./pages/Layout";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Dashboard />
+      </Layout>
+    ),
+  },
+  {
+    path: "/project",
+    element: (
+      <Layout>
+        <Project />
+      </Layout>
+    ),
+  },
+  {
+    path: "/group",
+    element: (
+      <Layout>
+        <Group />
+      </Layout>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import "./Dashboard.scss";
+
+function Dashboard() {
+  return (
+    <>
+      <h1>Studio Management Software</h1>
+    </>
+  );
+}
+
+export default Dashboard;
