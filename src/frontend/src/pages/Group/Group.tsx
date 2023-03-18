345f644036eb6924bc87de6854b00d9ffd4ad58c
William To <williamto222@gmail.com>
2023-03-18 16:20:35 +1100

    Added react routing and carbon design system

---
+++ src/frontend/src/pages/Group/Group.tsx
@@ -0,0 +1,22 @@
+import { Content } from "carbon-components-react";
+import AppHeader from "../../components/AppHeader";
+import { Breadcrumb, BreadcrumbItem } from "carbon-components-react";
+
+function Group() {
+  return (
+    <>
+      <div className="bx--row landing-page__banner">
+        <div className="bx--col-lg-16">
+          <Breadcrumb noTrailingSlash>
+            <BreadcrumbItem>
+              <a href="/">Project</a>
+            </BreadcrumbItem>
+          </Breadcrumb>
+          <h1 className="landing-page__heading">Groups</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Group;
---
+++ src/frontend/src/pages/Project/Project.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import AppHeader from "../../components/AppHeader";
+import { Content } from "carbon-components-react";
+
+function AboutPage() {
+  return (
+    <>
+      <section className="theme-section">
+        <h1>Projects</h1>
+      </section>
+    </>
+  );
+}
+
+export default AboutPage;
