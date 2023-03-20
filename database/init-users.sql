eda39de30b41b2402a666f72075952f40de28687
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-21 01:11:20 +1100

    added npm rev proxy

--- database/init-users.sql
+++ database/init-users.sql
@@ -5,15 +5,15 @@ create database if not exists NginxProxyManager;
 
 -- add the admin (with root access) user
 
-create user if not exists 'admin_user'@localhost identified by 'super_secret_password';
-grant ALL PRIVILEGES on *.* TO 'admin_user'@localhost IDENTIFIED BY 'super_secret_password';
+create user if not exists 'admin_user'@'%' identified by 'super_secret_password';
+grant ALL PRIVILEGES on *.* TO 'admin_user'@'%' IDENTIFIED BY 'super_secret_password';
 
 -- add the API user
 
-create user if not exists 'sms_user'@localhost identified by 'super_secret_password';
-grant ALL PRIVILEGES on StudioManagementSystem.* TO 'sms_user'@localhost IDENTIFIED BY 'super_secret_password';
+create user if not exists 'sms_user'@'%' identified by 'super_secret_password';
+grant ALL PRIVILEGES on StudioManagementSystem.* TO 'sms_user'@'%' IDENTIFIED BY 'super_secret_password';
 
 -- add the NPM (proxy) user
 
-create user if not exists 'npm_user'@localhost identified by 'super_secret_password';
-grant ALL PRIVILEGES on NginxProxyManager.* TO 'npm_user'@localhost IDENTIFIED BY 'super_secret_password';
+create user if not exists 'npm_user'@'%' identified by 'super_secret_password';
+grant ALL PRIVILEGES on NginxProxyManager.* TO 'npm_user'@'%' IDENTIFIED BY 'super_secret_password';
