b714c97a7bc100dd40f303a03e5c56fb906943e9
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-20 19:39:18 +1100

    added migration db context code

---
+++ src/server/StudioManagementSystem.Core/StudioManagementDbMigrationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudioManagementSystem.Core;
+
+public class StudioManagementDbMigrationContext : DbContext
+{
+    public StudioManagementDbMigrationContext(DbContextOptions opts) : base(opts) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        // new TestConfig().Configure(builder.Entity<Test>());
+    }
+}
