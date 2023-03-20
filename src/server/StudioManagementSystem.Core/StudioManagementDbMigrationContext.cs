a9a7c958bcfbbe6be87d0fd19de07c10fb09da81
Ryan Cleminson <rdcleminson@gmail.com>
2023-03-20 20:14:49 +1100

    Feature 7/docker containerisation (#30)
    Co-authored-by: theQuantumMechanic <albertferguson118@gmail.com>
    Co-authored-by: Ryan Cleminson <ry@Ryans-MBP-2.modem>

---
+++ src/server/StudioManagementSystem.Core/StudioManagementDbMigrationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudioManagementSystem.Core.Entities;
+
+namespace StudioManagementSystem.Core;
+
+public class StudioManagementDbMigrationContext : DbContext
+{
+    public StudioManagementDbMigrationContext(DbContextOptions opts) : base(opts) { }
+    public DbSet<MySpecialObject> MySpecialObjects => Set<MySpecialObject>();
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        new MySpecialObjectConfig().Configure(builder.Entity<MySpecialObject>());
+    }
+}
---
+++ src/server/StudioManagementSystem.Core/StudioManagementDbMigrationContextFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace StudioManagementSystem.Core;
+
+public class InMealDbMigrationContextFactory : IDesignTimeDbContextFactory<StudioManagementDbMigrationContext>
+{
+    // Holds migration infrastructure settings
+    private const string AppSettingsFilePath = "appsettings.json";
+
+    public StudioManagementDbMigrationContext CreateDbContext(string[] args)
+    {
+        Console.WriteLine("Starting migration...");
+
+        // configuration for a JSON settings file will not work without
+        // this package installed 'Microsoft.Extensions.Configuration.Json'
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(AppSettingsFilePath)
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("StudioManagementDbConnection");
+
+        Console.WriteLine($"Attempting to run a migration using the connection settings from '{connectionString}'");
+
+        var majorVersion = int.Parse(configuration.GetSection("ConnectionStrings:ServerVersionMajor").Value!);
+        var minorVersion = int.Parse(configuration.GetSection("ConnectionStrings:ServerVersionMinor").Value!);
+        var buildVersion = int.Parse(configuration.GetSection("ConnectionStrings:ServerVersionBuild").Value!);
+
+        var serverVersion = new MySqlServerVersion(new Version(majorVersion, minorVersion, buildVersion));
+
+        var dbContextBuilder =
+            new DbContextOptionsBuilder<StudioManagementDbMigrationContext>().UseMySql(connectionString!, serverVersion);
+
+        Console.WriteLine("created db context");
+
+        return new(dbContextBuilder.Options);
+    }
+}
