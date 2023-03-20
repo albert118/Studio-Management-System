655322612993e476d1010b8689ae63cdf9f5a939
Ryan Cleminson <ry@Ryans-MBP-2.modem>
2023-03-20 20:05:01 +1100

    added DB table and field context up and down

---
+++ src/server/StudioManagementSystem.Core/Migrations/20230320090056_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudioManagementSystem.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "MySpecialObject",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    SpecialField = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MySpecialObject", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MySpecialObject");
+        }
+    }
+}
--- src/server/StudioManagementSystem.Core/StudioManagementDbMigrationContext.cs
+++ src/server/StudioManagementSystem.Core/StudioManagementDbMigrationContext.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StudioManagementSystem.Core.Entities;
 
 namespace StudioManagementSystem.Core;
 
 public class StudioManagementDbMigrationContext : DbContext
 {
     public StudioManagementDbMigrationContext(DbContextOptions opts) : base(opts) { }
-
+    public DbSet<MySpecialObject> MySpecialObjects => Set<MySpecialObject>();
+    
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        // new TestConfig().Configure(builder.Entity<Test>());
+        new MySpecialObjectConfig().Configure(builder.Entity<MySpecialObject>());
     }
 }
