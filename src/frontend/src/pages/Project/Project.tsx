0ba4a310a651a8528d045ccf8415a14d6676c1c9
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-23 23:46:47 +1100

    removed dead imports

--- src/frontend/src/pages/Project/Project.tsx
+++ src/frontend/src/pages/Project/Project.tsx
@@ -1,15 +1,11 @@
-import React, { Component } from "react";
-import AppHeader from "../../components/AppHeader";
-import { Content } from "carbon-components-react";
-
 function AboutPage() {
-  return (
-    <>
-      <section className="theme-section">
-        <h1>Projects</h1>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className='theme-section'>
+				<h1>Projects</h1>
+			</section>
+		</>
+	);
 }
 
 export default AboutPage;
