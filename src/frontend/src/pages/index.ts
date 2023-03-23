b12a3ea65dda37db66f32375bd1e1ea5cfed4eb4
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-24 00:41:47 +1100

    simplify pages using index module

---
+++ src/frontend/src/pages/index.ts
@@ -0,0 +1,6 @@
+import Dashboard from 'pages/Dashboard/Dashboard';
+import Project from 'pages/Project/Project';
+import Group from 'pages/Group/Group';
+import Layout from 'pages/Layout';
+
+export { Dashboard, Project, Group, Layout };
