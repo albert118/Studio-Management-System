051f0ba238c569c82d9cac6cc7cd618f640a6086
William To <williamto222@gmail.com>
2023-03-24 03:13:47 +1100

    Added UTS Footer.

---
+++ src/frontend/src/components/AppFooter/AppFooter.tsx
@@ -0,0 +1,280 @@
+import "components/AppFooter/AppFooter.scss";
+import radial from "assets/radial.png";
+function AppFooter() {
+  return (
+    <div className="footer__main">
+      <div className="footer__wrapper">
+        <div className="acknowledgement">
+          <div className="grid__container">
+            <div className="acknowledgement__row">
+              <div className="acknowledgement__col acknowledgement__col__1">
+                <div className="acknowledgement__text">
+                  <div className="acknowledgement__text__header">
+                    Acknowledgement of Country
+                  </div>
+                  <p>
+                    UTS acknowledges the Gadigal people of the Eora Nation, the
+                    Boorooberongal people of the Dharug Nation, the Bidiagal
+                    people and the Gamaygal people, upon whose ancestral lands
+                    our university stands. We would also like to pay respect to
+                    the Elders both past and present, acknowledging them as the
+                    traditional custodians of knowledge for these lands.
+                  </p>
+                </div>
+              </div>
+              <div className="acknowledgement__col acknowledgement__col__2">
+                <img className="acknowledgement__media" alt="" src={radial} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="footer footer-grid__container">
+          <div className="footer-grid__row">
+            <div className="footer__first footer-grid__col footer-grid--col-4">
+              <div
+                id="block-footer"
+                className="block block-system block-system-menu-blockfooter"
+              >
+                <nav
+                  className="footer-menu"
+                  role="navigation"
+                  aria-label="Site information"
+                >
+                  <ul className="footer-menu__level-1">
+                    <li className="footer-menu__item">
+                      <a
+                        href="https://www.uts.edu.au/about"
+                        target=""
+                        rel=""
+                        data-drupal-link-system-path="node/683"
+                      >
+                        About UTS
+                      </a>
+                      <ul className="footer-menu__level-2">
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/about/university"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/684"
+                          >
+                            The University
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a href="http://maps.uts.edu.au" target="" rel="">
+                            Campus maps
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/about/uts-governance"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/704"
+                          >
+                            UTS governance
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/about/faculties"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/695"
+                          >
+                            Faculties
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/privacy-statement-footer"
+                            data-drupal-link-system-path="node/248336"
+                          >
+                            Privacy
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/about/utsweb-statements/accessibility-statement"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/4991"
+                          >
+                            Accessibility
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/about/utsweb-statements/disclaimer"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/4984"
+                          >
+                            Disclaimer
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li className="footer-menu__item">
+                      <a
+                        href="https://www.uts.edu.au/staff"
+                        target=""
+                        rel=""
+                        data-drupal-link-system-path="node/715"
+                      >
+                        Staff
+                      </a>
+                      <ul className="footer-menu__level-2">
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/staff-connect"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/91701"
+                          >
+                            Staff Connect
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/webmail"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/4570"
+                          >
+                            Webmail
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/staff-directory"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/4569"
+                          >
+                            Staff Directory
+                          </a>
+                        </li>
+                        <li className="footer-menu__item">
+                          <a
+                            href="https://www.uts.edu.au/current-vacancies"
+                            target=""
+                            rel=""
+                            data-drupal-link-system-path="node/247991"
+                          >
+                            Jobs at UTS
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+            <div className="footer__second footer-grid__col footer-grid--col-2">
+              <div
+                id="block-contactus"
+                className="block block-block-content block-block-content4b8321fa-fdec-446b-9b2e-d83b6f6b2e01"
+              >
+                <div className="field field-field-content field-type-text-long field-label-hidden">
+                  <div className="field-item">
+                    <h3>
+                      <a href="https://www.uts.edu.au/about/contacts/uts-contacts">
+                        Contact us
+                      </a>
+                    </h3>
+
+                    <p>
+                      <a href="https://www.uts.edu.au/current-students/managing-your-course/ask-uts">
+                        <strong>Student and public enquiries</strong>
+                      </a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div
+                id="block-sociallinks"
+                className="block block-system block-system-menu-blocksocial-links"
+              >
+                <nav
+                  className="footer-social"
+                  role="navigation"
+                  aria-label="UTS social media"
+                >
+                  <ul className="footer-social__menu">
+                    <li className="footer-social__item">
+                      <a
+                        href="http://facebook.com/UTSEngage"
+                        target=""
+                        rel=""
+                        className="facebook"
+                      >
+                        Facebook
+                      </a>
+                    </li>
+                    <li className="footer-social__item">
+                      <a
+                        href="https://twitter.com/utsengage"
+                        target=""
+                        rel=""
+                        className="twitter"
+                      >
+                        Twitter
+                      </a>
+                    </li>
+                    <li className="footer-social__item">
+                      <a
+                        href="http://instagram.com/utsengage"
+                        target=""
+                        rel=""
+                        className="instagram"
+                      >
+                        Instagram
+                      </a>
+                    </li>
+                    <li className="footer-social__item">
+                      <a
+                        href="http://youtube.com/user/utschannel"
+                        target=""
+                        rel=""
+                        className="youtube"
+                      >
+                        Youtube
+                      </a>
+                    </li>
+                    <li className="footer-social__item">
+                      <a
+                        href="http://linkedin.com/company/166678"
+                        target=""
+                        rel=""
+                        className="linkedin"
+                      >
+                        Linkedin
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div className="footer__third">
+            <div
+              id="block-authoriserblock"
+              className="block block-uts-article block-uts-article-authoriser-block"
+            >
+              <p>
+                © Copyright UTS - CRICOS Provider No: 00099F - 07 March 2023
+                03:23 PM. The page is authorised by the Chief Operating Officer
+                and Vice-President (COO).
+              </p>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default AppFooter;
