0d26f9c07215b2cbc1fa7aa9d3c9e3c517f9df7b
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-24 00:17:48 +1100

    added some extra config

---
+++ src/frontend/src/config/BrandConfig.js
@@ -0,0 +1,6 @@
+const config = Object.freeze({
+	BrandName: 'Studio Management System',
+	GitSocialLink: 'https://github.com/albert118/Studio-Management-System'
+});
+
+export default config;
---
+++ src/frontend/src/config/index.js
@@ -0,0 +1,6 @@
+import ApiConfig from './ApiConfig';
+import { routerConfig } from './RouterConfig';
+
+export { default } from './Config';
+
+export { ApiConfig, routerConfig };
