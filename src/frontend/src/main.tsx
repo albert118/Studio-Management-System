9d91fdfd3b6c57e1e91375f15927895ee5bf2b64
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-24 00:26:34 +1100

    neaten up

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-// import { routeConfig } from './config/RouteConfig';
 import { routeConfig } from 'config';
+
+import 'index.scss';
 
 const router = createBrowserRouter(routeConfig);
 
