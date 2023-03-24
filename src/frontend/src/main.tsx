14e8a0eb41dcee9e1084eb28048d59382684a541
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-24 20:12:40 +1100

    create a styles directory and add CSS resets

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from 'navigation/RouterConfig';
 
-import 'index.scss';
+import 'styles/index.scss';
 
 const router = createBrowserRouter(routes);
 
