24c576c2b20bf21d2b0d5678bde23023ac95f72b
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-25 03:23:18 +1100

    responsive and simple dashboard page

--- src/frontend/src/pages/Dashboard/Dashboard.tsx
+++ src/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -5,30 +5,33 @@ import { useNavigate } from 'react-router-dom';
 import AppRoutes from '~/navigation/AppRoutes';
 
 function Dashboard() {
-    return (
-        <Grid>
-            <Column lg={16} md={8} sm={4}>
-                <h1>{BrandConfig.BrandName}</h1>
-            </Column>
-            <Column lg={8} md={6} sm={4}>
-                <ProductDescription />
-            </Column>
-        </Grid>
-    );
-}
-
-function ProductDescription() {
     const navigate = useNavigate();
 
     return (
-        <div className='product-description'>
-            <h2>What is the {BrandConfig.BrandName}?</h2>
-            <p>
-                The {BrandConfig.BrandName} is a tool for student's and teachers that's designed to
-                streamline your experience and workflow throughout the semester and to the next.
-            </p>
-            <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
-        </div>
+        <Grid fullWidth>
+            <Column lg={16} md={8} sm={4} className='dashboard-page__banner'>
+                <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
+                {/* <div className='dashboard-page__login-promt'> */}
+                <p className='dashboard-page__p'>Existing user? Sign in here</p>
+                <Button
+                    className='dashboard-page__login'
+                    onClick={() => navigate(`${AppRoutes.about}`)}
+                >
+                    Login
+                </Button>
+            </Column>
+            <Column lg={16} md={8} sm={4} className='dashboard-page__description'>
+                <h2 className='dashboard-page__subheading '>
+                    What is the {BrandConfig.BrandName}?
+                </h2>
+                <p className='dashboard-page__p'>
+                    The {BrandConfig.BrandName} is a tool for student's and teachers that's designed
+                    to streamline your experience and workflow throughout the semester and to the
+                    next.
+                </p>
+                <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
+            </Column>
+        </Grid>
     );
 }
 
