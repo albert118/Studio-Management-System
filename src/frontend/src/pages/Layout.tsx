fe752e3461804bc7403230500eb2f4ba5139da90
William To <williamto222@gmail.com>
2023-03-24 19:26:32 +1100

    Refactor style format.

--- src/frontend/src/pages/Layout.tsx
+++ src/frontend/src/pages/Layout.tsx
@@ -1,19 +1,19 @@
-import AppHeader from "components/AppHeader/AppHeader";
-import AppFooter from "components/AppFooter/AppFooter";
-import { Content } from "carbon-components-react";
+import AppHeader from 'components/AppHeader/AppHeader';
+import AppFooter from 'components/AppFooter/AppFooter';
+import { Content } from 'carbon-components-react';
 
 interface Props {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 function Layouts(props: Props) {
-  return (
-    <div>
-      <AppHeader />
-      <Content>{props.children}</Content>
-      <AppFooter />
-    </div>
-  );
+	return (
+		<div>
+			<AppHeader />
+			<Content>{props.children}</Content>
+			<AppFooter />
+		</div>
+	);
 }
 
 export default Layouts;
--- src/frontend/src/pages/Project/Project.tsx
+++ src/frontend/src/pages/Project/Project.tsx
@@ -1,11 +1,11 @@
 function Project() {
-  return (
-    <>
-      <section className="theme-section">
-        <h1>Projects</h1>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className='theme-section'>
+				<h1>Projects</h1>
+			</section>
+		</>
+	);
 }
 
 export default Project;
