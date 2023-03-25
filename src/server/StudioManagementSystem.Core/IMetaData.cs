f535f7523904551d45bf1d0ace35c9eab5ce8380
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-25 16:48:53 +1100

    added interfaces for common data attributes

---
+++ src/server/StudioManagementSystem.Core/IMetaData.cs
@@ -0,0 +1,11 @@
+﻿namespace StudioManagementSystem.Core;
+
+/// <summary>
+/// Tags an entity with "meta data fields". These are common data fields, such as UpdateOn
+/// </summary>
+public interface IMetaData
+{
+    DateTime UpdatedOn { get; set; }
+
+    DateTime CreatedOn { get; set; }
+}
