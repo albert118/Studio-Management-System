7192f660793d6dde257f9019988c1d26ea3e52e4
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-25 17:16:34 +1100

    added project and project config

---
+++ src/server/StudioManagementSystem.Core/Entities/ProjectConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StudioManagementSystem.Core.Entities;
+
+public class ProjectConfig
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.ToTable(nameof(Project).ToLowerInvariant());
+
+        builder.Property(e => e.Title).IsRequired();
+        builder.Property(e => e.Description).IsRequired();
+
+        builder.ConfigureMetaData().ConfigureArchivable().ConfigureMetaData().ConfigureLockable();
+    }
+}
