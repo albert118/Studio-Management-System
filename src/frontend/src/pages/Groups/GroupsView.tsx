facc0ad0d37c20bc0b16223cc6ffa6b5aac7b67d
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-27 01:54:57 +1100

    fixed title and added dummy (UTS semester) session
    * session makes the page more useful

--- src/frontend/src/pages/Groups/GroupsView.tsx
+++ src/frontend/src/pages/Groups/GroupsView.tsx
@@ -17,10 +17,12 @@ import { GroupsDataTable } from './GroupsDataTable';
 export default function GroupsView({ groups }: IViewProps) {
     const navigate = useNavigate();
 
+    const currentSession = 'Autumn 2023';
+
     return (
         <Grid fullwidth>
             <Column lg={16} md={8} sm={4} className='groups-page__banner'>
-                <h1 className='groups-page__heading'>All Projects</h1>
+                <h1 className='groups-page__heading'>{currentSession} Groups</h1>
                 <p className='groups-page__p'>Browse all active groups</p>
                 <p className='groups-page__p'>
                     Looking to create a new group? Let's get you setup!
