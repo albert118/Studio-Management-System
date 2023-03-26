6c7e35a2405417e8be6f8f940fb6d50cf374b365
theQuantumMechanic <albertferguson118@gmail.com>
2023-03-27 01:12:06 +1100

    better test data with unique'ish IDs

--- src/frontend/src/pages/Groups/GroupsContainer.tsx
+++ src/frontend/src/pages/Groups/GroupsContainer.tsx
@@ -3,26 +3,92 @@ import { IGroup } from './types';
 
 import GroupsView from './GroupsView';
 
-const testGroupDto: IGroup = {
-    id: 'abc1234',
-    name: 'Group 03',
-    description: 'A test group',
-    MemberInfo: {
-        max: 6,
-        count: 3,
-        members: ['John', 'Agatha', 'Jenny']
+const dummyData: Array<IGroup> = [
+    {
+        id: 1,
+        name: 'Group 01',
+        description: 'A test group',
+        MemberInfo: {
+            max: 6,
+            count: 3,
+            members: ['John', 'Agatha', 'Jenny']
+        },
+        members: '3/6',
+        project: 'Apple Computers'
     },
-    members: '3/6',
-    project: 'Apple Workshop'
-};
-
-const createData = (id: string) => {
-    const _d = testGroupDto;
-    _d.id = id;
-    return _d;
-};
-
-const dummyData = [createData('123'), createData('456'), createData('789')];
+    {
+        id: 2,
+        name: 'Group 02',
+        description: 'A test group',
+        MemberInfo: {
+            max: 6,
+            count: 3,
+            members: ['John', 'Agatha', 'Jenny']
+        },
+        members: '3/6',
+        project: 'Applebees'
+    },
+    {
+        id: 3,
+        name: 'Group 03',
+        description: 'A test group',
+        MemberInfo: {
+            max: 6,
+            count: 2,
+            members: ['Agatha', 'Jenny']
+        },
+        members: '2/6',
+        project: 'Apple a day...'
+    },
+    {
+        id: 4,
+        name: 'Group 04',
+        description: 'A test group',
+        MemberInfo: {
+            max: 6,
+            count: 3,
+            members: ['John', 'Agatha', 'Jenny']
+        },
+        members: '3/6',
+        project: 'Apple "Think Different"'
+    },
+    {
+        id: 5,
+        name: 'Group 05',
+        description: 'A test group',
+        MemberInfo: {
+            max: 6,
+            count: 3,
+            members: ['John', 'Agatha', 'Jenny']
+        },
+        members: '3/6',
+        project: 'Apple iPhone'
+    },
+    {
+        id: 6,
+        name: 'Group 06',
+        description: 'A test group',
+        MemberInfo: {
+            max: 6,
+            count: 3,
+            members: ['John', 'Agatha', 'Jenny']
+        },
+        members: '3/6',
+        project: 'Apple Genius Bar'
+    },
+    {
+        id: 7,
+        name: 'Group 07',
+        description: 'A test group',
+        MemberInfo: {
+            max: 8,
+            count: 1,
+            members: ['Agatha']
+        },
+        members: '1/8',
+        project: 'Apple Lift'
+    }
+];
 
 export default function GroupsContainer() {
     const [data, useData] = useState<Array<IGroup>>(dummyData);
