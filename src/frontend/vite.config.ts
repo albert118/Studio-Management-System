5006e4e334f87b5ff99eb392b82355b8e104098f
William To <williamto222@gmail.com>
2023-04-02 15:05:34 +1000

    Added proxy for backend api server

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -4,5 +4,14 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths()]
+    plugins: [react(), tsconfigPaths()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://localhost:7230',
+                changeOrigin: true,
+                rewrite: path => path.replace(/^\/api/, '')
+            }
+        }
+    }
 });
