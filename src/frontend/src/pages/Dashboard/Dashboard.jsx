bed85068a81d97d33bc452e30127799f0b0ae133
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-12 00:53:39 +1000

    fix ~ import formatting

--- src/frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { BrandConfig } from 'config';
 import { Button, Grid, Column } from '@carbon/react';
 import { useNavigate } from 'react-router-dom';
-import AppRoutes from '~/navigation/AppRoutes';
+import AppRoutes from 'navigation/AppRoutes';
 import logo from 'assets/logo.jpg';
 
 function Dashboard() {
