c9c9dbff64384b990c23c0cc16db38e526b03eb6
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-12 00:12:28 +1000

    fixed various import bugs

--- src/frontend/src/pages/Layout.jsx
+++ src/frontend/src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import AppHeader from 'components/AppHeader/AppHeader';
 import AppFooter from 'components/AppFooter/AppFooter';
-import Content from '@carbon/react';
+import { Content } from '@carbon/react';
 
 function Layouts(props) {
     return (
