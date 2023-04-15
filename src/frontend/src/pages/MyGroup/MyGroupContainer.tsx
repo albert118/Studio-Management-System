a727b6c38d3b62af5fa18d72624879a60648b190
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-16 08:55:02 +1000

    GUID type checks

--- src/frontend/src/pages/MyGroup/MyGroupContainer.tsx
+++ src/frontend/src/pages/MyGroup/MyGroupContainer.tsx
@@ -1,16 +1,17 @@
 import MyGroupView from './MyGroupView';
 import { useGroup } from 'hooks/GroupHooks';
 import { useParams } from 'react-router-dom';
+import { Guid } from 'guid-typescript';
 
 export default function MyGroupContainer() {
     const { groupId } = useParams();
 
-    if (!groupId) {
+    if (!groupId || Guid.isGuid(groupId)) {
         // TODO: develop an error page and handle this better
         return <div>Error! No group ID</div>;
     }
 
-    const { group, updateGroup, isLoading } = useGroup(groupId);
+    const { group, updateGroup, isLoading } = useGroup(Guid.parse(groupId));
 
     // TODO: add a spinner
     return isLoading ? 'loading...' : <MyGroupView group={group} updateGroup={updateGroup} />;
