ad6af688a6a09c6c94e90f6ab092e430a4a707d9
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-16 10:25:27 +1000

    added and updated components

--- src/frontend/src/components/SimpleCard/SimpleCard.tsx
+++ src/frontend/src/components/SimpleCard/SimpleCard.tsx
@@ -1,17 +1,20 @@
+import { Size } from 'types/enums';
+
 export type Props = {
     title: string;
     label: string;
     children?: React.ReactNode;
+    size?: Size;
 };
 
-export default function SimpleCard(props: Props) {
+export default function SimpleCard({ title, label, size = Size.md, children }: Props) {
     return (
-        <div className='simple-card'>
+        <div className={`simple-card simple-card--${size.toString()}`}>
             <div>
-                <h5>{props.title}</h5>
-                {props.label}
+                <h5>{title}</h5>
+                {label}
             </div>
-            {props.children}
+            {children}
         </div>
     );
 }
--- src/frontend/src/components/index.tsx
+++ src/frontend/src/components/index.tsx
@@ -2,5 +2,7 @@ import VerticalDivider from './VerticalDivider/VerticalDivider';
 import GoToButton from './Buttons/GoTo';
 import SimpleCard from './SimpleCard/SimpleCard';
 import Stack from './Stack/Stack';
+import InlineDetail from './InlineDetail/InlineDetail';
+import BlockDetail from './BlockDetail/BlockDetail';
 
-export { Stack, GoToButton, VerticalDivider, SimpleCard };
+export { Stack, InlineDetail, BlockDetail, GoToButton, VerticalDivider, SimpleCard };
