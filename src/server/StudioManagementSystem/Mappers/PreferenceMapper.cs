f725339119783336d60f1c971ba195020f760fa2
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-19 05:48:58 +1000

    DTOs and mappers

---
+++ src/server/StudioManagementSystem/Mappers/PreferenceMapper.cs
@@ -0,0 +1,15 @@
+﻿using StudioManagementSystem.Core.Entities;
+
+namespace StudioManagementSystem.Mappers;
+
+public static class PreferenceMapper
+{
+    public static GroupProjectPreference MapToGroupProjectPreference(this KeyValuePair<Guid, int> preference, Group group)
+    {
+        return new() {
+            ProjectId = preference.Key,
+            Rank = preference.Value,
+            Group = group
+        };
+    }
+}
