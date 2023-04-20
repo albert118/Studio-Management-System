de225645dcb48ae0978392ea60d1a2f8cb249dbd
Ryan Cleminson <ry@Ryans-MBP-2.modem>
2023-04-20 16:59:36 +1000

    Initial create of owner contact hooks & container

--- src/frontend/src/pages/AddProject/AddProjectContainer.tsx
+++ src/frontend/src/pages/AddProject/AddProjectContainer.tsx
@@ -1,25 +1,12 @@
 import AddProjectView from './AddProjectView';
 import { PrincipalOwner } from 'types/types';
 import { Guid } from 'guid-typescript';
+import useOwnerContacts from 'hooks/ContactHooks';
 
-const dummyOwners: PrincipalOwner[] = [
-    {
-        id: Guid.parse('6476324e-0c56-45e4-bd28-5c10dd5fda1c'),
-        name: 'Jeremy Clarkson',
-        email: 'jeremyclarkson@topgear.bbc'
-    },
-    {
-        id: Guid.parse('820cdb59-798a-43f1-9a48-d93de82e6985'),
-        name: 'Richard Hammond',
-        email: 'richardhammon@topgear.bbc'
-    },
-    {
-        id: Guid.parse('c14abb6e-37c0-4383-a3c4-36d78ce8e320'),
-        name: 'James May',
-        email: 'jamesmay@topgear.bbc'
-    }
-];
+const dummyid = '18674a20-e0ef-40a2-aae6-559f01ef3138'
 
 export default function AddProjectContainer() {
-    return <AddProjectView availableOwners={dummyOwners} />;
+    const { ownerContacts } = useOwnerContacts([Guid.parse(dummyid)]);
+
+    return <AddProjectView availableOwners={ownerContacts} />;
 }
