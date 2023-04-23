2f63e10363ca552dd15f473ec49b13652f9107e2
Jayden Lee <Jayden.Lee@student.uts.edu.au>
2023-04-23 14:18:07 +1000

    test

--- src/frontend/src/pages/Login/Login.tsx
+++ src/frontend/src/pages/Login/Login.tsx
@@ -3,7 +3,7 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { supabase } from 'main';
 
 function Login() {
-    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}></Auth>;
+    return <div className='loginClass'><Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}></Auth></div>;
 }
 
 export default Login;
