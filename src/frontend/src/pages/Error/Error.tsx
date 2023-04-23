54a22d69840c41a3dabfdccfae402cac225681dc
William To <williamto222@gmail.com>
2023-04-24 00:58:21 +1000

    Redesign 404 page

--- src/frontend/src/pages/Error/Error.tsx
+++ src/frontend/src/pages/Error/Error.tsx
@@ -1,11 +1,25 @@
 function Error() {
-	return (
-		<div>
-			<h1> 404 Page Not Found</h1>
-			<br></br>
-			<p> The page you wish to access is unreachable.</p>
-		</div>
-	);
+    return (
+        <div className='flex-container'>
+            <div className='text-center'>
+                <h1>
+                    <span className='fade-in' id='digit1'>
+                        4
+                    </span>
+                    <span className='fade-in' id='digit2'>
+                        0
+                    </span>
+                    <span className='fade-in' id='digit3'>
+                        4
+                    </span>
+                </h1>
+                <h3 className='fadeIn'>PAGE NOT FOUND</h3>
+                <button type='button' name='button'>
+                    Return To Home
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Error;
