6ac8db027bccd7955a7fed8c0f1f15f3735a233f
Ryan Cleminson <ry@Ryans-MBP-2.modem>
2023-04-24 22:21:34 +1000

    Initial Formatting Adjustment

--- src/frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { BrandConfig } from 'config';
-import { Button, Grid, Column } from '@carbon/react';
+import { Button, Grid, Column} from '@carbon/react';
 import { useNavigate } from 'react-router-dom';
 import AppRoutes from 'navigation/AppRoutes';
 import logo from 'assets/logo.jpg';
@@ -10,31 +10,37 @@ function Dashboard() {
     return (
         <Grid>
             <Column lg={16} md={8} sm={4} className='dashboard-page__banner'>
-                <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
-                <p className='dashboard-page__p'>Existing user? Sign in here</p>
-                <Button
-                    className='dashboard-page__login'
-                    onClick={() => navigate(`${AppRoutes.login}`)}
-                >
-                    Login
-                </Button>
+                <div className='dashboard-page__banner_content'>
+                    <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
+                    <p className='dashboard-page__p'>Existing user? Sign in here</p>
+                    <Button
+                        className='dashboard-page__login'
+                        onClick={() => navigate(`${AppRoutes.login}`)}
+                    >
+                        Login
+                    </Button>
+                </div>
             </Column>
 
             <Column lg={16} md={8} sm={4}>
                 <Grid>
-                    <Column md={4} lg={7} sm={4} className='dashboard-page__description'>
-                        <h2 className='dashboard-page__subheading '>
-                            What is the {BrandConfig.BrandName}?
-                        </h2>
-                        <p className='dashboard-page__p'>
-                            The {BrandConfig.BrandName} is a tool for student's and teachers that's
-                            designed to streamline your experience and workflow throughout the
-                            semester and to the next.
-                        </p>
-                        <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
+                    
+                    <Column md={4} lg={9} sm={4} className='dashboard-page__description'>
+                        <div className='dashboard-page__description_content'>
+                            <h2 className='dashboard-page__subheading '>
+                                What is the {BrandConfig.BrandName}?
+                            </h2>
+                            <p className='dashboard-page__p'>
+                                The {BrandConfig.BrandName} is a tool for student's and teachers that's
+                                designed to streamline your experience and workflow throughout the
+                                semester and to the next.
+                            </p>
+                            <br/>
+                            <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
+                        </div>
                     </Column>
-                    <Column md={7} lg={{ span: 8, offset: 7 }} sm={4}>
-                        <img className='dashboard-page__logo' src={logo} alt='studio word art' />
+                    <Column md={5} lg={{ span: 8, offset: 9 }} sm={4}>
+                        <img className='dashboard-page__logo' src={logo} alt='studio word art'/>
                     </Column>
                 </Grid>
             </Column>
