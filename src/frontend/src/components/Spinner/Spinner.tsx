b7799833f25073e50ba4daa3d05e10e5155e5dc7
Ryan Cleminson <ry@Ryans-MBP-2.modem>
2023-04-25 17:23:22 +1000

    Styling update to finish it off

--- src/frontend/src/components/Spinner/Spinner.tsx
+++ src/frontend/src/components/Spinner/Spinner.tsx
@@ -1,20 +1,18 @@
-import { ThreeCircles } from  'react-loader-spinner'
+import { ThreeCircles } from 'react-loader-spinner'
 
 export default function LoadingSpinner() {
     return (
-        <div className={`loading-spinner`} >
+        <div style={{ "display": "flex" }} >
             <ThreeCircles
-            height="150"
-            width="150"
-            wrapperStyle={{
-                "margin": "auto",
-                "padding":"10%",
-            }}
-            wrapperClass="loading-spinner circles"
-            ariaLabel="three-circles-rotating"
-            outerCircleColor="#bdbdbd"
-            innerCircleColor="#0f62fe"
-            middleCircleColor="#bdbdbd"
+                height="150"
+                width="150"
+                wrapperStyle={{
+                    "margin": "auto",
+                    "padding": "10%",
+                }}
+                outerCircleColor="#bdbdbd"
+                innerCircleColor="#0f62fe"
+                middleCircleColor="#bdbdbd"
             />
         </div>
     );
