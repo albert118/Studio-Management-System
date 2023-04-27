c8701e199ba604c54a3d7406b24f4fc67dc2f865
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-27 22:35:39 +1000

    added hooks and UI container changes

--- src/frontend/src/pages/AddProject/AddProjectContainer.tsx
+++ src/frontend/src/pages/AddProject/AddProjectContainer.tsx
@@ -1,12 +1,8 @@
 import AddProjectView from './AddProjectView';
-import { PrincipalOwner } from 'types/types';
-import { Guid } from 'guid-typescript';
 import useOwnerContacts from 'hooks/ContactHooks';
 
-const dummyid = '18674a20-e0ef-40a2-aae6-559f01ef3138'
-
 export default function AddProjectContainer() {
-    const { ownerContacts } = useOwnerContacts([Guid.parse(dummyid)]);
+    const { ownerContacts } = useOwnerContacts();
 
     return <AddProjectView availableOwners={ownerContacts} />;
 }
