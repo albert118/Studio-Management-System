524cf01dddd218d4b3477681b8676c8d2b5f84de
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-27 22:42:46 +1000

    remove null setter on Messages

--- src/server/StudioManagementSystem.Core/Entities/GroupApplication.cs
+++ src/server/StudioManagementSystem.Core/Entities/GroupApplication.cs
@@ -12,7 +12,7 @@ public class GroupApplication: IMetaData
 
     public StudentContact Contact { get; set; } = null!;
     
-    public string? Messages { get; set; } = null!;
+    public string? Messages { get; set; }
     
     public DateTime UpdatedOn { get; set; }
     
