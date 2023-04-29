6e73a76e063c10bc81ed75882e3ba0acda6c65df
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-30 02:13:26 +1000

    design and styling of admin page

--- src/frontend/src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/frontend/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,5 +1,4 @@
-import { Tile, Grid, Column, ModalWrapper, Button } from '@carbon/react';
-import { Stack } from 'components';
+import { Tile, Grid, Column, ModalWrapper } from '@carbon/react';
 
 export default function AdminDashboard() {
     return (
@@ -13,26 +12,77 @@ export default function AdminDashboard() {
                 </p>
             </Column>
             <Column lg={16} md={8} sm={4} className='admin-page__r2'>
-                <Tile className='stat-box'>#1</Tile>
-                <Tile className='stat-box'>#2</Tile>
-                <Tile className='stat-box'>#3</Tile>
+                <StatisticBox title='Assigned Groups' label='12 out of 30' variant='green' />
+                <StatisticBox title='Assigned Groups' label='12 out of 30' variant='blue' />
+                <StatisticBox title='Assigned Groups' label='12 out of 30' />
             </Column>
             <Column lg={16} md={8} sm={4} className='admin-page__r3'>
                 <ManagementTile title='Groups' description='Archive, and lock groups in bulk'>
-                    <Button>#1</Button>
-                    <Button>#2</Button>
-                    <Button>#3</Button>
+                    <ModalWrapper
+                        buttonTriggerText='Add new owner contacts'
+                        modalHeading='Create new contacts'
+                    >
+                        {/* <PendingApplications groupApplications={groupApplication} /> */}
+                    </ModalWrapper>
+
+                    <ModalWrapper
+                        buttonTriggerText='Bulk group management'
+                        modalHeading='Bulk group management'
+                    >
+                        {/* <PendingApplications groupApplications={groupApplication} /> */}
+                    </ModalWrapper>
+                </ManagementTile>
+                <ManagementTile title='Projects' description='Archive, and lock projects in bulk'>
+                    <ModalWrapper
+                        buttonTriggerText='Bulk project management'
+                        modalHeading='Bulk project management'
+                    >
+                        {/* <PendingApplications groupApplications={groupApplication} /> */}
+                    </ModalWrapper>
+                </ManagementTile>
+                <ManagementTile
+                    title='Group-Project Assignment'
+                    description='Manage groups project assignement'
+                >
+                    <ModalWrapper
+                        buttonTriggerText='Auto assign projects'
+                        modalHeading='Auto assign projects'
+                    >
+                        {/* <PendingApplications groupApplications={groupApplication} /> */}
+                    </ModalWrapper>
+
+                    <ModalWrapper
+                        buttonTriggerText='Manually assign projects'
+                        modalHeading='Manually assign projects'
+                    >
+                        {/* <PendingApplications groupApplications={groupApplication} /> */}
+                    </ModalWrapper>
                 </ManagementTile>
             </Column>
         </Grid>
     );
 }
 
+function StatisticBox({ title, label, variant }) {
+    const classes = variant ? `statistic-box ${variant}` : 'statistic-box';
+
+    return (
+        <Tile className={classes}>
+            <div className='heading'>
+                <h4>{title}</h4>
+                <label>{label}</label>
+            </div>
+        </Tile>
+    );
+}
+
 function ManagementTile({ title, description, children }) {
     return (
         <div className='management-tile'>
-            <h3>{title}</h3>
-            <p>{description}</p>
+            <div className='heading'>
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
             <div className='actions'>{children}</div>
         </div>
     );
