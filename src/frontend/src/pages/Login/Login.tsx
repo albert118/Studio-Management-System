d2fe819bd34632234e7a1d331889ec51fc61cf92
theQuantumMechanic <albertferguson118@gmail.com>
2023-04-29 23:16:33 +1000

    cleaned up and simpified the page imports

--- src/frontend/src/pages/Login/Login.tsx
+++ src/frontend/src/pages/Login/Login.tsx
@@ -2,8 +2,6 @@ import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { supabase } from 'main';
 
-function Login() {
+export default function Login() {
     return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]}></Auth>;
 }
-
-export default Login;
