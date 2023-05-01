cc8f59c1e7c88a992adc8c75a7e24733ed3d8750
Ryan Cleminson <ry@Ryans-MBP-2.modem>
2023-05-01 17:13:11 +1000

    Boarder radii removal

--- src/frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -41,39 +41,6 @@ function Dashboard() {
                 </Column>
             </Grid>
 
-            {/* <Grid className='dashboard-page-elements'>
-                <Column lg={16} md={8} sm={4} className='dashboard-page__hero_banner'>
-                    <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
-                    <p className='dashboard-page__p'>Existing user? Sign in here</p>
-                    <Button
-                        className='dashboard-page__login'
-                        onClick={() => navigate(`${AppRoutes.login}`)}
-                    >
-                        Login
-                    </Button>
-                    
-                </Column>
-                
-                <Column lg={16} md={8} sm={4} className='dashboard-page__banner'>
-                    <Grid>
-                        <Column md={4} lg={7} sm={4} className='dashboard-page__description'>
-                            <h2 className='dashboard-page__subheading '>
-                                What is the {BrandConfig.BrandName}?
-                            </h2>
-                            <p className='dashboard-page__p'>
-                                The {BrandConfig.BrandName} is a tool for student's and teachers that's
-                                designed to streamline your experience and workflow throughout the
-                                semester and to the next.
-                            </p>
-                            <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
-                        </Column>
-                        <Column md={7} lg={{ span: 8, offset: 7 }} sm={4}>
-                            <img className='dashboard-page__logo' src={logo} alt='studio word art' />
-                        </Column>
-                    </Grid>
-                </Column>
-                
-            </Grid> */}
         </div>
     );
 }
