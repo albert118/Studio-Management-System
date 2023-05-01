459a09dcc0755b5e5640654b208b6e0b3045a3c8
Ryan Cleminson <ry@Ryans-MBP-2.modem>
2023-05-01 17:00:11 +1000

    UI Upate to footer, header and some pages

--- src/frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -3,26 +3,28 @@ import { Button, Grid, Column } from '@carbon/react';
 import { useNavigate } from 'react-router-dom';
 import AppRoutes from 'navigation/AppRoutes';
 import logo from 'assets/logo.jpg';
+import {Container, Row, Col} from 'react-bootstrap';
+
 
 function Dashboard() {
     const navigate = useNavigate();
+    // This page may require restructuring however its current purpose is to act as a template dashboard page.
 
     return (
-        <Grid>
-            <Column lg={16} md={8} sm={4} className='dashboard-page__banner'>
-                <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
-                <p className='dashboard-page__p'>Existing user? Sign in here</p>
-                <Button
-                    className='dashboard-page__login'
-                    onClick={() => navigate(`${AppRoutes.login}`)}
-                >
-                    Login
-                </Button>
-            </Column>
-
-            <Column lg={16} md={8} sm={4}>
-                <Grid>
-                    <Column md={4} lg={7} sm={4} className='dashboard-page__description'>
+        <div className='dashboard-page'>
+            <Grid>
+                <Column lg={10} md={8} sm={4} className='dashboard-page-title'>
+                    <Column className='dashboard-page__content01'>
+                        <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
+                        <p className='dashboard-page__p'>Existing user? Sign in here</p>
+                        <Button
+                            className='dashboard-page__login'
+                            onClick={() => navigate(`${AppRoutes.login}`)}
+                        >
+                            Login
+                        </Button>
+                    </Column>
+                    <Column className='dashboard-page__content02'>
                         <h2 className='dashboard-page__subheading '>
                             What is the {BrandConfig.BrandName}?
                         </h2>
@@ -33,12 +35,46 @@ function Dashboard() {
                         </p>
                         <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
                     </Column>
-                    <Column md={7} lg={{ span: 8, offset: 7 }} sm={4}>
-                        <img className='dashboard-page__logo' src={logo} alt='studio word art' />
-                    </Column>
-                </Grid>
-            </Column>
-        </Grid>
+                </Column>
+                <Column md={7} lg={{span: 6}} sm={4} className='dashboard-page-img'>
+                    <img className='dashboard-page__logo' src={logo} alt='studio word art' />
+                </Column>
+            </Grid>
+
+            {/* <Grid className='dashboard-page-elements'>
+                <Column lg={16} md={8} sm={4} className='dashboard-page__hero_banner'>
+                    <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
+                    <p className='dashboard-page__p'>Existing user? Sign in here</p>
+                    <Button
+                        className='dashboard-page__login'
+                        onClick={() => navigate(`${AppRoutes.login}`)}
+                    >
+                        Login
+                    </Button>
+                    
+                </Column>
+                
+                <Column lg={16} md={8} sm={4} className='dashboard-page__banner'>
+                    <Grid>
+                        <Column md={4} lg={7} sm={4} className='dashboard-page__description'>
+                            <h2 className='dashboard-page__subheading '>
+                                What is the {BrandConfig.BrandName}?
+                            </h2>
+                            <p className='dashboard-page__p'>
+                                The {BrandConfig.BrandName} is a tool for student's and teachers that's
+                                designed to streamline your experience and workflow throughout the
+                                semester and to the next.
+                            </p>
+                            <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
+                        </Column>
+                        <Column md={7} lg={{ span: 8, offset: 7 }} sm={4}>
+                            <img className='dashboard-page__logo' src={logo} alt='studio word art' />
+                        </Column>
+                    </Grid>
+                </Column>
+                
+            </Grid> */}
+        </div>
     );
 }
 
--- src/frontend/src/pages/Projects/ProjectsView.jsx
+++ src/frontend/src/pages/Projects/ProjectsView.jsx
@@ -11,25 +11,27 @@ export default function ProjectsView({ projects }) {
     const currentSession = 'Autumn 2023';
 
     return (
-        <Grid>
-            <Column lg={16} md={8} sm={4} className='projects-page__banner'>
-                <h1 className='projects-page__heading'>{currentSession} Projects</h1>
-                <p className='projects-page__p'>Browse all active projects</p>
-                <p className='projects-page__p'>
-                    Looking to create a new project? Let's get you setup!
-                </p>
-                {/* remove this once the admin interface is added */}
-                <Button
-                    className='projects-page__new-project-prompt'
-                    onClick={() => navigate(`${AppRoutes.projects}/add`)}
-                    kind='ghost'
-                >
-                    (*temp) Create a new project
-                </Button>
-            </Column>
-            <Column lg={16} md={8} sm={4} className='projects-page__datatable'>
-                <ProjectsDataTable projects={projects} />
-            </Column>
-        </Grid>
+        <div className='projects-page'>
+            <Grid>
+                <Column lg={16} md={8} sm={4} className='projects-page__banner'>
+                    <h1 className='projects-page__heading'>{currentSession} Projects</h1>
+                    <p className='projects-page__p'>Browse all active projects</p>
+                    <p className='projects-page__p'>
+                        Looking to create a new project? Let's get you setup!
+                    </p>
+                    {/* Remove this once the admin interface is added */}
+                    <Button
+                        className='projects-page__new-project-prompt'
+                        onClick={() => navigate(`${AppRoutes.projects}/add`)}
+                        kind='ghost'
+                    >
+                        (*temp) Create a new project
+                    </Button>
+                </Column>
+                <Column lg={16} md={8} sm={4} className='projects-page__datatable'>
+                    <ProjectsDataTable projects={projects} />
+                </Column>
+            </Grid>
+        </div>
     );
 }
