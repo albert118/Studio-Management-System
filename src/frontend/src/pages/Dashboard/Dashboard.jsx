f68e66ed30e305fb7d39cea4f80b51f425c85b7f
theQuantumMechanic <albertferguson118@gmail.com>
2023-05-04 01:36:47 +1000

    cleaned up dashboard layout and design

--- src/frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,40 +1,36 @@
+import { Roadmap } from '@carbon/icons-react';
 import { BrandConfig } from 'config';
 import { Button, Grid, Column } from '@carbon/react';
 import { useNavigate } from 'react-router-dom';
 import AppRoutes from 'navigation/AppRoutes';
-import logo from 'assets/logo.jpg';
 
 export default function Dashboard() {
     const navigate = useNavigate();
-    // This page may require restructuring however its current purpose is to act as a template home page.
 
     return (
         <Grid className='dashboard-page'>
-            <Column lg={10} md={8} sm={4} className='dashboard-page__title'>
-                <Column className='dashboard-page__content01'>
-                    <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
-                    <p className='dashboard-page__p'>Existing user? Sign in here</p>
-                    <Button
-                        className='dashboard-page__login'
-                        onClick={() => navigate(`${AppRoutes.login}`)}
-                    >
-                        Login
-                    </Button>
-                </Column>
-                <Column className='dashboard-page__content02'>
-                    <h2 className='dashboard-page__subheading '>
-                        What is the {BrandConfig.BrandName}?
-                    </h2>
-                    <p className='dashboard-page__p'>
-                        The {BrandConfig.BrandName} is a tool for student's and teachers that's
-                        designed to streamline your experience and workflow throughout the semester
-                        and to the next.
-                    </p>
-                    <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
-                </Column>
+            <Column lg={6} md={6} sm={4} className='dashboard-page__pitch'>
+                <h1>
+                    <div>Manage Studio Groups</div>
+                    <div>and projects</div>
+                    <div>
+                        all in one place <Roadmap size={48} />
+                    </div>
+                </h1>
+                <p className='dashboard-page__pitch-detail'>
+                    Designed for students and teachers, the {BrandConfig.BrandName} is a tool to
+                    streamline your studio workflow experience throughout the semester and to the
+                    next.
+                </p>
+
+                <Button onClick={() => navigate(`${AppRoutes.about}`)}>Get started</Button>
             </Column>
-            <Column md={7} lg={{ span: 6 }} sm={4} className='dashboard-page__hero'>
-                <img className='dashboard-page__logo' src={logo} alt='studio word art' />
+            <Column lg={{ span: 10 }} md={8} sm={4} className='dashboard-page__hero'>
+                <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
+            </Column>
+            <Column className='dashboard-page__existinguser'>
+                <p className='dashboard-page__existinguser-prompt'>Existing user? Sign in here</p>
+                <Button onClick={() => navigate(`${AppRoutes.login}`)}>Login</Button>
             </Column>
         </Grid>
     );
