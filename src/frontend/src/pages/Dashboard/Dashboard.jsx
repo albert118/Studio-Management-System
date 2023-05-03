647b46cf89bfb3a9ab1bd3f708d879329615ed95
theQuantumMechanic <albertferguson118@gmail.com>
2023-05-04 02:21:10 +1000

    add simple hero design

--- src/frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -10,13 +10,13 @@ export default function Dashboard() {
     return (
         <Grid className='dashboard-page'>
             <Column lg={6} md={6} sm={4} className='dashboard-page__pitch'>
-                <h1>
+                <h2>
                     <div>Manage Studio Groups</div>
                     <div>and projects</div>
                     <div>
                         all in one place <Roadmap size={48} />
                     </div>
-                </h1>
+                </h2>
                 <p className='dashboard-page__pitch-detail'>
                     Designed for students and teachers, the {BrandConfig.BrandName} is a tool to
                     streamline your studio workflow experience throughout the semester and to the
@@ -25,8 +25,20 @@ export default function Dashboard() {
 
                 <Button onClick={() => navigate(`${AppRoutes.about}`)}>Get started</Button>
             </Column>
-            <Column lg={{ span: 10 }} md={8} sm={4} className='dashboard-page__hero'>
-                <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
+            <Column lg={{ span: 8, offset: 8 }} md={6} sm={4} className='dashboard-page__hero'>
+                <div className='dashboard-page__hero-boxes'>
+                    <div className='box1' />
+                    <div className='box2' />
+                    <div className='box3' />
+                </div>
+
+                <h1>
+                    <div>Studio Management</div>
+                    <div>
+                        <div>System</div>
+                        <div className='divider' />
+                    </div>
+                </h1>
             </Column>
             <Column className='dashboard-page__existinguser'>
                 <p className='dashboard-page__existinguser-prompt'>Existing user? Sign in here</p>
