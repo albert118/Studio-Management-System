643b637665d3d134d47e6cdc1718b06b6daeb474
Jayden Lee <jayden@rlc.com.au>
2023-05-04 17:29:14 +1000

    addressing alberts commitments

--- src/frontend/src/pages/Login/Login.tsx
+++ src/frontend/src/pages/Login/Login.tsx
@@ -3,23 +3,24 @@ import { ThemeSupa } from '@supabase/auth-ui-shared';
 import useAuth from 'hooks/AuthHooks';
 import { supabase } from 'main';
 import AppRoutes from 'navigation/AppRoutes';
-import { Navigate } from 'react-router-dom';
+import { redirect } from "react-router-dom";
 
-function Login() {
+
+export function Login() {
     const { session } = useAuth();
     // I want auto redirect but it's not working
     if (!session) {
         return (
-            <div className='loginClass'>
+            <div className='login-form'>
                 <Auth
                     supabaseClient={supabase}
                     appearance={{ theme: ThemeSupa }}
                     providers={[]}
-                    redirectTo={`${AppRoutes.root}`}
+                    redirectTo={AppRoutes.root}
                 ></Auth>
             </div>
         );
     } else {
-        return <Navigate replace to='/' />;
+        return redirect(`${AppRoutes.root}`)
     }
 }
