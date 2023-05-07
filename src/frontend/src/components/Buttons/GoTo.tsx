d5c14b6c0c8776181616c9b468156cb4b7129a93
theQuantumMechanic <albertferguson118@gmail.com>
2023-05-07 22:51:52 +1000

    Feature/branding and theme (better hero icon and CTA's)  (#108)
    Co-authored-by: Ryan Cleminson <rdcleminson@gmail.com>

--- src/frontend/src/components/Buttons/GoTo.tsx
+++ src/frontend/src/components/Buttons/GoTo.tsx
@@ -5,15 +5,17 @@ import { useNavigate } from 'react-router-dom';
 
 export interface IComponentProps {
     url: string;
+    text?: string;
+    size?: string;
 }
 
-export default function GoToButton({ url }: IComponentProps) {
+export default function GoToButton({ url, text = 'Go to', size = 'lg' }: IComponentProps) {
     const navigate = useNavigate();
 
     return (
         // @ts-ignore
-        <Button onClick={() => navigate(url)} renderIcon={ArrowRight}>
-            Go to
+        <Button size={size} onClick={() => navigate(url)} renderIcon={ArrowRight}>
+            {text}
         </Button>
     );
 }
--- src/frontend/src/pages/Dashboard/Dashboard.jsx
+++ src/frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,40 +1,47 @@
+import { Roadmap } from '@carbon/icons-react';
 import { BrandConfig } from 'config';
-import { Button, Grid, Column } from '@carbon/react';
-import { useNavigate } from 'react-router-dom';
+import { Grid, Column } from '@carbon/react';
 import AppRoutes from 'navigation/AppRoutes';
-import logo from 'assets/logo.jpg';
+import { GoToButton, SimpleCard } from 'components';
 
 export default function Dashboard() {
-    const navigate = useNavigate();
-    // This page may require restructuring however its current purpose is to act as a template home page.
-
     return (
         <Grid className='dashboard-page'>
-            <Column lg={10} md={8} sm={4} className='dashboard-page__title'>
-                <Column className='dashboard-page__content01'>
-                    <h1 className='dashboard-page__heading'>{BrandConfig.BrandName}</h1>
-                    <p className='dashboard-page__p'>Existing user? Sign in here</p>
-                    <Button
-                        className='dashboard-page__login'
-                        onClick={() => navigate(`${AppRoutes.login}`)}
-                    >
-                        Login
-                    </Button>
-                </Column>
-                <Column className='dashboard-page__content02'>
-                    <h2 className='dashboard-page__subheading '>
-                        What is the {BrandConfig.BrandName}?
-                    </h2>
-                    <p className='dashboard-page__p'>
-                        The {BrandConfig.BrandName} is a tool for student's and teachers that's
-                        designed to streamline your experience and workflow throughout the semester
-                        and to the next.
-                    </p>
-                    <Button onClick={() => navigate(`${AppRoutes.about}`)}>Read more</Button>
-                </Column>
+            <Column lg={6} md={6} sm={4} className='dashboard-page__pitch'>
+                <h2>
+                    <div>Manage Studio Groups</div>
+                    <div>and projects</div>
+                    <div>
+                        all in one place <Roadmap size={48} />
+                    </div>
+                </h2>
+                <p className='dashboard-page__pitch-detail'>
+                    Designed for students and teachers, the {BrandConfig.BrandName} is a tool to
+                    streamline your studio workflow experience throughout the semester and to the
+                    next.
+                </p>
+
+                <GoToButton url={`${AppRoutes.groups}/add`} text='Get started' size='xl' />
             </Column>
-            <Column md={7} lg={{ span: 6 }} sm={4} className='dashboard-page__hero'>
-                <img className='dashboard-page__logo' src={logo} alt='studio word art' />
+            <Column lg={{ span: 8, offset: 8 }} md={6} sm={4} className='dashboard-page__hero'>
+                <div className='dashboard-page__hero-boxes'>
+                    <div className='box1' />
+                    <div className='box2' />
+                    <div className='box3' />
+                </div>
+
+                <h1>
+                    <div>Studio Management</div>
+                    <div>
+                        <div>System</div>
+                        <div className='divider' />
+                    </div>
+                </h1>
+            </Column>
+            <Column lg={16} md={8} sm={44} className='dashboard-page__existing-prompt'>
+                <SimpleCard title='Existing user?' label='Sign in here'>
+                    <GoToButton url={AppRoutes.login} text='Login' />
+                </SimpleCard>
             </Column>
         </Grid>
     );
